08fa339dc1a1162b82e58676fcb677c271de9a2f
Thonio <anthonio.rj@hotmail.fr>
2025-06-10 16:46:40 +0300

    design sketchybar

--- sketchybar/items/front_app.sh
+++ sketchybar/items/front_app.sh
@@ -13,6 +13,9 @@ front_app=(
 
 sketchybar --add event window_focus \
   --add event windows_on_spaces \
+  windows_on_spaces \
+  mouse.clicked \
+  \
   --add item front_app left \
   --set front_app "${front_app[@]}" \
   --subscribe front_app front_app_switched
