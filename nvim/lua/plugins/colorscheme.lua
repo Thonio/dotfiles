3ff96c279d7000a279bb2b5a0898b2502f6cd9ea
Thonio <anthonio@etalik.com>
2026-04-28 11:24:48 +0300

    change theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,43 @@
 return {
-  "tokyonight.nvim",
+  "f4z3r/gruvbox-material.nvim",
+  name = "gruvbox-material",
+  lazy = false,
+  priority = 1000,
   opts = {
-    transparent = true,
-    style = "night",
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
+    background = {
+      transparent = true,
+    },
+    contrast = "medium",
+    float = {
+      force_background = false,
+    },
+    signs = {
+      force_background = false,
     },
   },
+  config = function(_, opts)
+    require("gruvbox-material").setup(opts)
+    vim.cmd.colorscheme("gruvbox-material")
+
+    local transparent_groups = {
+      "Normal",
+      "NormalNC",
+      "NormalFloat",
+      "FloatBorder",
+      "SignColumn",
+      "EndOfBuffer",
+    }
+
+    local function apply_transparency()
+      for _, group in ipairs(transparent_groups) do
+        vim.api.nvim_set_hl(0, group, { bg = "none" })
+      end
+    end
+
+    apply_transparency()
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      pattern = "gruvbox-material",
+      callback = apply_transparency,
+    })
+  end,
 }
